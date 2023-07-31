fbc47305e39e3a07b6655583cd6bc6f77e587800
junios <junios@naver.com>
2023-07-31 11:34:21 +0900

    SDL2 초기화 코드 추가

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using SDL2;
+using System;
 
 namespace L20230725
 {
@@ -8,12 +9,29 @@ namespace L20230725
         {
             SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
 
-            SDL.SDL_CreateWindow("Game", 100, 100, 640, 480, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
-
-            while(true)
+            IntPtr myWindow = SDL.SDL_CreateWindow("Game", 100, 100, 640, 480, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+            IntPtr myRenderer = SDL.SDL_CreateRenderer(myWindow, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED
+                | SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC
+                | SDL.SDL_RendererFlags.SDL_RENDERER_TARGETTEXTURE);
+            SDL.SDL_Event myEvent;
+            bool isRunning = true;
+            while(isRunning)
             {
+                SDL.SDL_PollEvent(out myEvent);
+                switch(myEvent.type)
+                {
+                    case SDL.SDL_EventType.SDL_QUIT:
+                        isRunning = false;
+                        break;
+                }
 
+                SDL.SDL_SetRenderDrawColor(myRenderer, 0, 0, 0, 0);
+                SDL.SDL_RenderClear(myRenderer);
+                SDL.SDL_RenderPresent(myRenderer);
             }
+
+            SDL.SDL_DestroyRenderer(myRenderer);
+            SDL.SDL_DestroyWindow(myWindow);
 
             SDL.SDL_Quit();
         }
